3c2e3b25bec26989730288a7389d38b51808ba34
Deepak Talwar <deepaktalwardt@gmail.com>
2020-03-08 19:23:56 -0700

    Added inifinite loop

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_COMM_SIZE 255
 #define MAX_ARGS 8
@@ -11,34 +12,37 @@ int main(int argc, char *argv[], char* envp[]) {
     // envp - environment pointer 
     // TODO: Should roshell inherit envp from parent shells?
 
-
     // print out all the environment variables
-    for(int i=0; envp[i]; ++i)
+    for(int i = 0; envp[i]; ++i)
     {
         printf("environment variable %d: %s \n", i, envp[i]);
     }
 
+    while (1)
+    {
+        char input[MAX_COMM_SIZE + 1] = { 0x0 };
+        char* args[MAX_ARGS + 1] = { NULL };
+        char* ptr = input;
 
-    char input[MAX_COMM_SIZE + 1] = { 0x0 };
-    char* args[MAX_ARGS + 1] = { NULL };
-    char* ptr = input;
+        printf("user@computer $:");
+        fgets(input, MAX_COMM_SIZE, stdin);
+        input[strlen(input) - 1] = '\0'; //terminate with null, rather than with \n
 
-    printf("user@computer $:");
-    fgets(input, MAX_COMM_SIZE, stdin);
-    input[strlen(input) - 1] = '\0'; //terminate with null, rather than with \n
+        // taken from: https://danrl.com/blog/2018/how-to-write-a-tiny-shell-in-c/
+        // TODO: use strtok() instead
+        for (int i = 0; i < sizeof(args) && *ptr; ptr++) {
+            if (*ptr == ' ') continue;
+            if (*ptr == '\n') break;
+            for (args[i++] = ptr; *ptr && *ptr != ' ' && *ptr != '\n'; ptr++);
+            *ptr = '\0';
+        }
 
-
-    // taken from: https://danrl.com/blog/2018/how-to-write-a-tiny-shell-in-c/
-    // TODO: use strtok() instead
-    for (int i = 0; i < sizeof(args) && *ptr; ptr++) {
-        if (*ptr == ' ') continue;
-        if (*ptr == '\n') break;
-        for (args[i++] = ptr; *ptr && *ptr != ' ' && *ptr != '\n'; ptr++);
-        *ptr = '\0';
+        if (fork() == 0) // if inside the child process
+        {
+            exit(execvp(args[0], args));
+        }
+        wait(NULL);
     }
-
-    execvp(args[0], args);
-
 }
 
 
