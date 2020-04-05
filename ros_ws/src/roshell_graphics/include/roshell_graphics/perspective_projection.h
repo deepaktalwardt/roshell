606dde4d2ea02fc9894029dbb87c501cf606a6a1
Deepak Talwar <deepaktalwardt@gmail.com>
2020-04-04 19:42:02 -0700

    Added perspective projection library

--- ros_ws/src/roshell_graphics/include/roshell_graphics/perspective_projection.h
+++ ros_ws/src/roshell_graphics/include/roshell_graphics/perspective_projection.h
@@ -3,58 +3,103 @@
 #include <iostream>
 #include <string>
 #include <Eigen/Dense>
+#include "math.h"
 
 #include "roshell_graphics.h"
 
 namespace roshell_graphics
 {
 
-using Point3D = Eigen::Vector3i;
+#define PI            3.14159265
+#define RAD_TO_DEG    PI/180.0
+#define DEG_TO_RAD    180.0/PI
 
-typedef struct Camera
+/****************
+ * Camera struct
+ ****************/
+struct Camera
 {
-    Point3D location;     // as defined in the world frame
+    Eigen::Vector3f location;     // as defined in the world frame
     int focal_distance;   // in meters
 };
 
+/******************
+ * Transform Class
+ ******************/
 class Transform
 {
     public:
         // Assuming Z-axis points to world origin
-        Transform(Eigen::Vector3f origin);
+        Transform(
+            const Camera& cam);
 
-        Transform(Eigen::Vector3f origin, Eigen::Matrix3Xf rotation_matrix);
-        Transform(Eigen::Vector3f origin, float roll, float pitch, float yaw);
+        Transform(
+            const Eigen::Vector3f& origin);
+        
+        ~Transform();
 
-        Eigen::Matrix3Xf get_rotation_matrix() const;
+        Eigen::Matrix3f get_rotation_matrix() const;
         Eigen::Vector3f get_origin() const;
+        Eigen::Matrix4f get_transformation_matrix() const;
 
-        Eigen::Matrix3Xf convert_angles_to_rotation_matrix(float roll, float pitch, float yaw);
+        Eigen::Matrix3f angles_to_rotation_matrix(
+            const float& theta, 
+            const float& phi);
+        
+        void update(const Eigen::Vector3f& origin);
 
     private:
         Eigen::Vector3f origin_;
-        Eigen::Matrix3Xf rotation_matrix_;
+        Eigen::Matrix3f rotation_matrix_;
 
-};
+        float rho_;
+        Eigen::Matrix4f T_;
 
-Transform::Transform(Eigen::Vector3f origin, Eigen::Matrix3Xf rotation_matrix):
-    origin_(origin),
-    rotation_matrix_(rotation_matrix)
+};  // Transform class
+
+Transform::Transform(const Camera& cam)
+{
+    update(cam.location);
+}
+
+Transform::Transform(const Eigen::Vector3f& origin)
+{
+    update(origin);
+}
+
+void Transform::update(const Eigen::Vector3f& origin)
+{
+    origin_ = origin;
+
+    rho_ = sqrt(pow(origin_(0), 2) + pow(origin_(1), 2) + pow(origin_(2), 2));
+
+    float phi = acos(origin_(2) / rho_);
+    float theta = asin(origin_(1) / sqrt(pow(origin_(0), 2) + pow(origin_(1), 2))); 
+
+    rotation_matrix_ = angles_to_rotation_matrix(theta, phi);
+
+    T_.block<3, 3>(0, 0) = rotation_matrix_;
+
+    Eigen::Vector3f last_col(0, 0, rho_);
+    Eigen::Vector4f last_row(0, 0, 0, 1);
+
+    T_.block<3, 1>(0, 3) = last_col;
+    T_.block<1, 4>(3, 0) = last_row;
+}
+
+Transform::~Transform()
 {
 }
 
-Transform::Transform(Eigen::Vector3f origin, float roll, float pitch, float yaw):
-    origin_(origin)
-{
-    rotation_matrix_ = convert_angles_to_rotation_matrix(roll, pitch, yaw);
-}
-
-Eigen::Matrix3Xf Transform::convert_angles_to_rotation_matrix(float roll, float pitch, float yaw)
+Eigen::Matrix3f Transform::angles_to_rotation_matrix(
+    const float& theta,
+    const float& phi)
 {   
-    Eigen::Matrix3Xf rot_mat;
-    rot_mat << 1, 0, 0,
-               0, 1, 0,
-               0, 0, 1;
+    Eigen::Matrix3f rot_mat;
+    rot_mat << -sin(theta),             cos(theta),                 0,
+               -cos(phi)*cos(theta),  -cos(phi)*sin(theta),     sin(phi),
+               -sin(phi)*cos(theta),  -sin(phi)*cos(theta),     -cos(phi);
+
     return rot_mat;
 }
 
@@ -63,27 +108,82 @@ Eigen::Vector3f Transform::get_origin() const
     return origin_;
 }
 
-Eigen::Matrix3Xf Transform::get_rotation_matrix() const
+Eigen::Matrix3f Transform::get_rotation_matrix() const
 {
     return rotation_matrix_;
 }
 
+Eigen::Matrix4f Transform::get_transformation_matrix() const
+{
+    return T_;
+}
 
+/******************************
+ * PerspectiveProjection Class
+ ******************************/
 class PerspectiveProjection
 {
     public:
-        PerspectiveProjection(Camera camera, Transform tf);
+        PerspectiveProjection(const Camera& camera);
         ~PerspectiveProjection();
 
-        void update_camera(Camera camera);
+        void update_camera(const Camera& camera);
 
-        Point3D transform_coordinate(const Point3D& point);
-        Point3D transform_coordinate(const Point3D& point, const Transform& tf);
+        Eigen::Vector3f transform_world_point(const Eigen::Vector3f& point_in_world_frame);
+        Eigen::Vector2f project_world_point(const Eigen::Vector3f& point_in_world_frame);
+        Eigen::Vector2f project_cam_point(const Eigen::Vector3f& point_in_cam_frame);
 
     private:
         Camera camera_;
         Transform tf_;
+
 };  // class PerspectiveProjection
+
+PerspectiveProjection::PerspectiveProjection(const Camera& camera):
+    camera_(camera),
+    tf_(camera)
+{
+}
+
+PerspectiveProjection::~PerspectiveProjection()
+{
+}
+
+Eigen::Vector3f PerspectiveProjection::transform_world_point(
+    const Eigen::Vector3f& point_in_world_frame)
+{
+    Eigen::Vector4f point_in_aug, point_out_aug;
+    point_in_aug << point_in_world_frame, 1;
+    point_out_aug = tf_.get_transformation_matrix() * point_in_aug;
+    
+    Eigen::Vector3f point_out_in_cam_frame = point_out_aug.block<3, 1>(0, 0);
+    return point_out_in_cam_frame;
+}
+
+Eigen::Vector2f PerspectiveProjection::project_world_point(
+    const Eigen::Vector3f& point_in_world_frame)
+{
+    Eigen::Vector3f point_in_cam_frame = transform_world_point(point_in_world_frame);
+    return project_cam_point(point_in_cam_frame);
+}
+
+Eigen::Vector2f PerspectiveProjection::project_cam_point(
+    const Eigen::Vector3f& point_in_cam_frame)
+{
+    Eigen::Vector2f point_in_image_plane;
+    point_in_image_plane(0) = point_in_cam_frame(0) * (camera_.focal_distance / point_in_cam_frame(2));
+    point_in_image_plane(1) = point_in_cam_frame(1) * (camera_.focal_distance / point_in_cam_frame(2));
+
+    // Fix for lower vertical resolution
+    point_in_image_plane(1) = 0.5 * point_in_image_plane(1);
+
+    return point_in_image_plane;
+}
+
+void PerspectiveProjection::update_camera(const Camera& camera)
+{
+    tf_.update(camera.location);
+}
 
 }  // namespace roshell_graphics
 
--- ros_ws/src/roshell_graphics/src/roshell_graphics_test_node.cpp
+++ ros_ws/src/roshell_graphics/src/roshell_graphics_test_node.cpp
@@ -32,17 +32,175 @@ void draw_random_lines(
     }
 }
 
+void draw_test(
+    roshell_graphics::RoshellGraphics& rg,
+    roshell_graphics::PerspectiveProjection& pp)
+{
+    Eigen::Vector3f origin_world(10, 0, 0);
+    Eigen::Vector3f origin_world_in_cam_frame =
+        pp.transform_world_point(origin_world);
+
+    std::cout << "origin_world_in_cam_frame " << std::endl <<
+        origin_world_in_cam_frame << std::endl;
+    
+    Eigen::Vector2f origin_world_in_image_frame =
+        pp.project_cam_point(origin_world_in_cam_frame);
+    
+    std::cout << "origin_world_in_image_frame " << std::endl <<
+        origin_world_in_image_frame << std::endl;
+    
+}
+
+void draw_axis(
+    roshell_graphics::RoshellGraphics& rg,
+    roshell_graphics::PerspectiveProjection& pp)
+{
+    Eigen::Matrix3Xf points_in_world_frame(3, 4);
+
+    points_in_world_frame.col(0) << 0, 0, 0;
+    points_in_world_frame.col(1) << 1000, 0, 0;
+    points_in_world_frame.col(2) << 0, 1000, 0;
+    points_in_world_frame.col(3) << 0, 0, 1000;
+
+    Eigen::Matrix2Xf points_in_image_frame(2, 4);
+    for (int i = 0; i < 4; i++)
+    {
+        points_in_image_frame.col(i) =
+            pp.project_world_point(points_in_world_frame.col(i));
+    }
+
+    roshell_graphics::Point p1, p2, p3, p4;
+
+    p1.first = points_in_image_frame(0, 0);
+    p1.second = points_in_image_frame(1, 0);
+
+    p2.first = points_in_image_frame(0, 1);
+    p2.second = points_in_image_frame(1, 1);
+
+    p3.first = points_in_image_frame(0, 2);
+    p3.second = points_in_image_frame(1, 2);
+
+    p4.first = points_in_image_frame(0, 3);
+    p4.second = points_in_image_frame(1, 3);
+
+    rg.fix_frame(p1);
+    rg.fix_frame(p2);
+    rg.fix_frame(p3);
+    rg.fix_frame(p4);
+
+    rg.line(p1, p2);
+    rg.line(p1, p3);
+    rg.line(p1, p4);
+
+    rg.draw();
+}
+
+void draw_rotating_cube(
+    roshell_graphics::RoshellGraphics& rg)
+{
+    // Vertices for the cube
+    Eigen::Matrix3Xf points_in_world_frame(3, 8);
+
+    // Cube length
+    float cl = 300;
+    points_in_world_frame.col(0) << 0,   0,   0;
+    points_in_world_frame.col(1) << cl,  0,   0;
+    points_in_world_frame.col(2) << cl,  cl,  0;
+    points_in_world_frame.col(3) << 0 ,  cl,  0;
+
+    points_in_world_frame.col(4) << 0 ,  0,  cl;
+    points_in_world_frame.col(5) << cl,  0,  cl;
+    points_in_world_frame.col(6) << cl,  cl, cl;
+    points_in_world_frame.col(7) << 0 ,  cl, cl;
+    
+    Eigen::Matrix2Xf points_in_image_frame(2, 8);
+
+    roshell_graphics::Camera cam;
+    Eigen::Vector3f cam_loc(8000, 10000, 5000);
+    cam.location = cam_loc;
+    cam.focal_distance = 3000;
+
+    roshell_graphics::PerspectiveProjection pp(cam);
+
+    for (float angle = 0.0; angle < 3.1415; angle += 0.1)
+    {   
+        roshell_graphics::Camera new_cam = cam;
+        float new_cam_x = cam_loc(0) * cos(angle);
+        float new_cam_y = cam_loc(1) * sin(angle);
+
+        Eigen::Vector3f new_cam_loc(new_cam_x, new_cam_y, cam_loc(2));
+        new_cam.location = new_cam_loc; 
+
+        pp.update_camera(new_cam);
+
+        for (int i = 0; i < 8; i++)
+        {
+            points_in_image_frame.col(i) =
+                pp.project_world_point(points_in_world_frame.col(i));
+        }
+
+        roshell_graphics::Point p1, p2, p3, p4, p5, p6, p7, p8;
+        p1.first = points_in_image_frame(0, 0);
+        p1.second = points_in_image_frame(1, 0);
+
+        p2.first = points_in_image_frame(0, 1);
+        p2.second = points_in_image_frame(1, 1);
+
+        p3.first = points_in_image_frame(0, 2);
+        p3.second = points_in_image_frame(1, 2);
+
+        p4.first = points_in_image_frame(0, 3);
+        p4.second = points_in_image_frame(1, 3);
+
+        p5.first = points_in_image_frame(0, 4);
+        p5.second = points_in_image_frame(1, 4);
+
+        p6.first = points_in_image_frame(0, 5);
+        p6.second = points_in_image_frame(1, 5);
+
+        p7.first = points_in_image_frame(0, 6);
+        p7.second = points_in_image_frame(1, 6);
+
+        p8.first = points_in_image_frame(0, 7);
+        p8.second = points_in_image_frame(1, 7);
+
+        rg.fix_frame(p1);
+        rg.fix_frame(p2);
+        rg.fix_frame(p3);
+        rg.fix_frame(p4);
+        rg.fix_frame(p5);
+        rg.fix_frame(p6);
+        rg.fix_frame(p7);
+        rg.fix_frame(p8);
+
+        // draw_axis(rg, pp);
+
+        rg.line(p1, p2);
+        rg.line(p2, p3);
+        rg.line(p3, p4);
+        rg.line(p4, p1);
+
+        rg.line(p5, p6);
+        rg.line(p6, p7);
+        rg.line(p7, p8);
+        rg.line(p8, p5);
+
+        rg.line(p1, p5);
+        rg.line(p2, p6);
+        rg.line(p3, p7);
+        rg.line(p4, p8);
+
+        rg.draw_and_clear(10e4);
+    }
+}
+
 int main(int argc, char** argv)
 {
     roshell_graphics::RoshellGraphics rg;
     std::pair<int, int> term_size = rg.get_terminal_size();
-    draw_random_lines(rg, 10, term_size.first, term_size.second, 5e5);
+    // draw_random_lines(rg, 10, term_size.first, term_size.second, 5e5);
 
-    Eigen::Vector3i cam_loc(10, 10, 10);
-    roshell_graphics::Camera cam;
-    cam.location = cam_loc;
-
-    roshell_graphics::Transform tf(cam_loc);
+    draw_rotating_cube(rg);
 
     return 0;
 }
