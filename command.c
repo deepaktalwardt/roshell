8518a1bd120fca8e3f9dde6c1c88b0ba2a564de2
Hyejun <leehj825@gmail.com>
2020-04-05 17:13:08 -0700

    Source command exit for binary file

--- command.c
+++ command.c
@@ -14,7 +14,7 @@ void executeCommand(char* input)
 
   char* args[MAX_ARGS + 1] = { NULL };
   input[strlen(input) - 1] = '\0'; //terminate with null, rather than with \n
-  
+
   char* token = strtok(input, " ");
   for(int i=0; token != NULL && i<MAX_ARGS; ++i)
   {
@@ -51,7 +51,7 @@ void executeCommand(char* input)
 
         if(comm_res == -1)  //execvp encountered error
         {
-            printf("Command '%s' exited with the following error: %s \n", args[0], strerror(errno));  
+            printf("Command '%s' exited with the following error: %s \n", args[0], strerror(errno));
             exit(-1);
         }
         else exit(0);
