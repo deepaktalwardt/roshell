8405813e12f513088910453b2f33fd2f8f588b41
Hyejun Lee <hyejun@ilb001078vm.ad.shared>
2020-05-15 13:09:23 -0700

    add "-l" option to rosbag to run in loop

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ cd ros_ws
 catkin_make
 source devel/setup.bash
 gnome-terminal -x "roscore"
-gnome-terminal -e "rosbag play test_bag_filtered.bag"
+gnome-terminal -e "rosbag play test_bag_filtered.bag -l"
 cd ..
 make
 ./roshell
